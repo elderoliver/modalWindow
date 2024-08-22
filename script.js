bc690603a8fe488b12a0d02376d62cb8aebb921f
Elder Oliveira <elderoliveiracastro>
2024-08-22 14:36:00 -0300

    first Commit -m

---
+++ script.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btn = document.querySelector('.close-modal');
+const showModal = document.querySelectorAll('.show-modal');
