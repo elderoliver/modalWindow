6a850d41080ec86669e57c4ffa9d3e58f14be308
Elder Oliveira <elderoliveiracastro>
2024-08-22 16:07:26 -0300

    add the events

--- script.js
+++ script.js
@@ -2,5 +2,35 @@
 
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
-const btn = document.querySelector('.close-modal');
+const btnCloseModal = document.querySelector('.close-modal');
 const showModal = document.querySelectorAll('.show-modal');
+
+for (let i = 0; i < showModal.length; i++) {
+  showModal[i].addEventListener('click', function () {
+    openCloseModal(1);
+  });
+}
+
+btnCloseModal.addEventListener('click', function () {
+  openCloseModal(0);
+});
+
+overlay.addEventListener('click', function () {
+  openCloseModal(0);
+});
+
+const openCloseModal = function (openClose) {
+  if (openClose === 1) {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  } else if (openClose === 0) {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+  }
+};
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    openCloseModal(0);
+  }
+});
